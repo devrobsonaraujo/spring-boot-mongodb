448a400a78ff9202ebe98e45352c74fcf0ec34f6
devrobsonaraujo <108498541+devrobsonaraujo@users.noreply.github.com>
2024-06-17 22:24:27 -0300

    first commit

---
+++ src/main/java/com/teste/mongodb/robsonaraujo/RobsonaraujoApplication.java
@@ -0,0 +1,13 @@
+package com.teste.mongodb.robsonaraujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobsonaraujoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobsonaraujoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/mongodb/robsonaraujo/RobsonaraujoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.mongodb.robsonaraujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobsonaraujoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
